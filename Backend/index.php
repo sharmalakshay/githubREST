5eef3c08bdb5d90e33014119a91cbaea64db1129
Lakshay Sharma <lsharma@tcd.ie>
2018-07-23 00:51:17 +0100

    Added funtionality: commits by users in submitted repos

--- Backend/index.php
+++ Backend/index.php
@@ -12,10 +12,13 @@ $context = stream_context_create(
 $allusers = array();
 
 $totalcommitsbyusers = array();
+$totalcommitsbytargetusers = array();
 
 $repos = explode(',', $_POST['repos']); //all repos in array
 
 echo "<b>Submitted repos are:</b><br>";
+
+$json = array();
 
 foreach($repos as $repo){
 	echo explode('/',$repo)[1]," by ",explode('/',$repo)[0],"<br>";
@@ -23,8 +26,8 @@ foreach($repos as $repo){
 	//$url0 = "https://api.github.com/repos/".trim($repo)."/contributors";
 	
 	$url = "https://api.github.com/repos/".trim($repo)."/commits";
-	$json = json_decode(file_get_contents($url, false, $context),true);
-	foreach($json as $commit){
+	$json[$repo] = json_decode(file_get_contents($url, false, $context),true);
+	foreach($json[$repo] as $commit){
 		$year = substr($commit['commit']['author']['date'],0,4);
 		if($year==2018){
 			$user = $commit['author']['login'];
@@ -36,7 +39,7 @@ foreach($repos as $repo){
 
 echo "<br><b>All users who contributed to any of the above in year 2018 are:</b><br>";
 foreach($allusers as $username){
-	//echo $username,"<br>";
+	echo $username,"<br>";
 	$url2 = "https://github.com/search/?q=author%3A$username&type=Commits";
 	$webpage = file_get_contents($url2, false, $context);
 	
@@ -45,10 +48,32 @@ foreach($allusers as $username){
 	$final = explode(">",$halfhalf[1]);
 	$totalcommits = explode("<",$final[1]);
 	//echo $totalcommits[0], "<br><br>";
-	$totalcommitsbyusers["$username"] = $totalcommits[0];
-	
+	$totalcommitsbyusers[$username] = $totalcommits[0];
 }
 
-print_r($totalcommitsbyusers);
+echo "<br><b>Most active to least active on Github among the above users:</b><br>";
+arsort($totalcommitsbyusers);
+foreach($totalcommitsbyusers as $key => $value){
+	echo "$key having $value commits<br>";
+}
 
+foreach($allusers as $theuser){
+	$totalcommitsbytargetusers[$theuser] = 0;
+}
+
+foreach($repos as $rep){	
+	foreach($json[$rep] as $repojson => $repocommits){
+		foreach($allusers as $targetuser){
+			if($repocommits['author']['login']==$targetuser){
+				$totalcommitsbytargetusers[$targetuser] += 1;
+			}
+		}
+	}
+}
+
+echo "<br><b>Most to least active in submitted repos</b> (Only verified commits)<b>:</b><br>";
+arsort($totalcommitsbytargetusers);
+foreach($totalcommitsbytargetusers as $target => $score){
+	echo "$target having $score commits<br>";
+}
 ?>
